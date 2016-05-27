e2f4e8019077a35e5bb5efcf0678ac58e69d8e17
Asif Iqbal <asif.31.iqbal@gmail.com>
2016-05-27 18:34:29 -0400

    Initial commit

---
+++ src/main/java/stormtest/LogAnalyserStorm.java
@@ -0,0 +1,34 @@
+package stormtest;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.utils.Utils;
+
+public class LogAnalyserStorm {
+    public static void main(String[] args) throws Exception{
+        //Create Config instance for cluster configuration
+        Config config = new Config();
+        config.setDebug(true);
+          
+        //
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("call-log-reader-spout", new FakeCallLogReaderSpout());
+
+        builder.setBolt("call-log-creator-bolt", new CallLogCreatorBolt())
+           .shuffleGrouping("call-log-reader-spout");
+
+        builder.setBolt("call-log-counter-bolt", new CallLogCounterBolt())
+           .fieldsGrouping("call-log-creator-bolt", new Fields("call"));
+              
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("LogAnalyserStorm", config, builder.createTopology());
+        Utils.sleep(10000);
+          
+        //Stop the topology
+        cluster.killTopology("LogAnalyserStorm");
+          
+        cluster.shutdown();
+     }
+}
